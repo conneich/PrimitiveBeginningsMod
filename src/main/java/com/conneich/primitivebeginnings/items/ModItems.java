f2b832eccd989a64269f70d568e4600df88965ce
conneich <conneich@gmail.com>
2022-06-06 14:28:02 -0500

    Initial commit

---
+++ src/main/java/com/conneich/primitivebeginnings/items/ModItems.java
@@ -0,0 +1,21 @@
+package com.conneich.primitivebeginnings.items;
+
+import com.conneich.primitivebeginnings.PrimitiveBeginningsMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(
+            ForgeRegistries.ITEMS, PrimitiveBeginningsMod.MODID
+    );
+
+
+
+
+    public static void register(IEventBus eventBus)
+    {
+        ITEMS.register(eventBus);
+    }
+}
